32f4e751ee0ef437b69953a1c922cbb4e841d517
hirosume <richardepope@my.smccd.edu>
2019-09-20 13:43:53 +0000

    fix redis url

--- server/shareds/sessionParser.js
+++ server/shareds/sessionParser.js
@@ -1,13 +1,15 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const dev = process.env.NODE_ENV !== 'production';
+const { DOCKER, REDIS_URL, REDIS_DOCKER_URL, NODE_ENV } = process.env;
+const dev = NODE_ENV !== 'production';
+const docker = DOCKER === '1';
 if (dev) {
 	module.exports = session({
 		secret: 'hirosume',
 		saveUninitialized: false,
 		resave: true,
 		store: new RedisStore({
-			url: process.env.REDIS_URL,
+			url: docker ? REDIS_DOCKER_URL : REDIS_URL,
 		}),
 	});
 } else {
